65617684808dfe4ece0e13e597b8f6e23766a75f
Ajayi Michael <ajayimichael150@gmail.com>
2023-08-09 13:13:33 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+import React from "react";
 import Info from "./component/info";
 import Nav from "./component/nav";
 import Map from "./component/map";
 
-
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
       <Nav />
-      <main className="main-section">
-        <Info />
-        <Map />
-      </main>
 
+      <main>
+        <div className="main-section">
+          <Info />
+          <Map />
+        </div>
+      </main>
     </div>
   );
 }
