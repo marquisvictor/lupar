30d577ca12b475eea14dc649602b3b76acb5e8d4
Ajayi Michael <ajayimichael150@gmail.com>
2023-08-09 13:13:33 +0100

    fix

--- src/component/map.jsx
+++ src/component/map.jsx
@@ -1,26 +1,28 @@
-import { useEffect, useRef } from "react"
-import mapboxgl from '!mapbox-gl'
-mapboxgl.accessToken = 'pk.eyJ1IjoidmlyZWtzIiwiYSI6ImNsbDAwcG8xNDFxa3AzbW1hMnNyM3gwNXYifQ.fjhylwF_ayrfb2I0ymjNFg'
+import React, { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoidmlyZWtzIiwiYSI6ImNsbDAwcG8xNDFxa3AzbW1hMnNyM3gwNXYifQ.fjhylwF_ayrfb2I0ymjNFg";
 
 function Map() {
-    const mapContainer = useRef(null)
-    useEffect(() => { 
-        const map = new mapboxgl.Map({
-            container: mapContainer.current,
-            zoom: 1.5,
-            center: [5,34],
-            style: 'mapbox://styles/mapbox/streets-v11'
+  const mapContainer = useRef(null);
 
-        })
-        return () => map.remove()
-    }, [])
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      zoom: 1.5,
+      center: [5, 34],
+      style: "mapbox://styles/mapbox/streets-v11",
+    });
+
+    return () => map.remove();
+  }, []);
+
   return (
-    <div>
-        <div ref={mapContainer} className="map-container">
-            
-        </div>
+    <div className="map-container">
+      <div ref={mapContainer} className="map" />
     </div>
-  )
+  );
 }
 
-export default Map
+export default Map;
