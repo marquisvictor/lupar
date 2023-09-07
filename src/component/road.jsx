80400dfc118e389aa03d865084eceb9a1bc79780
Ajayi Michael <ajayimichael150@gmail.com>
2023-09-07 12:08:38 +0100

    Partly done

--- src/component/road.jsx
+++ src/component/road.jsx
@@ -1,4 +1,3 @@
-import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import React, { useRef } from "react";
 
@@ -6,104 +5,92 @@ function Road({ properties }) {
   const tableContent = useRef();
 
   const downloadPdf = () => {
-    const tableInput = tableContent.current;
+    const tableInput = tableContent.current.innerHTML;
+    const pdf = new jsPDF("p", "pt", "a4", true);
 
-    html2canvas(tableInput).then((canvas) => {
-      const imgData = canvas.toDataURL("image/png");
-      const pdf = new jsPDF("p", "mm", "a4", true);
-      const pdfWidth = pdf.internal.pageSize.getWidth();
-      const pdfHeight = pdf.internal.pageSize.getHeight();
-      const imgWidth = canvas.width;
-      const imgHeight = canvas.height;
-      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
-      const imgX = (pdfWidth - imgWidth * ratio) / 2;
-      const imgY = 30;
+    var getContent = `<div style='font-size:11px; padding: 05px 15px; width:565.28px;'>${tableInput}</div>`;
 
-      pdf.addImage(
-        imgData,
-        "PNG",
-        imgX,
-        imgY,
-        imgWidth * ratio,
-        imgHeight * ratio
-      );
-      pdf.save(`${properties?.LABEL} Road Properties`);
+    pdf.html(getContent, {
+      async callback(pdf) {
+        // save the document as a PDF with name of pdf_name
+        pdf.save(`${properties?.LABEL}`);
+      },
     });
   };
 
   return (
-    <div ref={tableContent} className="section-container">
-      <div className="section-heading">{properties?.LABEL} Road Properties</div>
-      <div className="section-table">
-        <table>
-          <thead>
-            <tr>
-              <th scope="col">Property</th>
-              <th scope="col">Details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {properties?.Category_A && (
-              <tr>
-                <th scope="row">Road Category</th>
-                <td>{properties?.Category_A}</td>
-              </tr>
-            )}
-
-            {properties?.drainage_a && (
-              <tr>
-                <th scope="row">Drainage</th>
-                <td>{properties?.drainage_a}</td>
-              </tr>
-            )}
-
-            {properties?.LABEL && (
-              <tr>
-                <th scope="row">Label</th>
-                <td>{properties?.LABEL}</td>
-              </tr>
-            )}
-
-            {properties?.road_condi && (
-              <tr>
-                <th scope="row">Road Condition</th>
-                <td>{properties?.road_condi}</td>
-              </tr>
-            )}
-
-            {properties?.zone_name && (
-              <tr>
-                <th scope="row">Zone name</th>
-                <td>{properties?.zone_name}</td>
-              </tr>
-            )}
-
-            {properties?.supervisor && (
-              <tr>
-                <th scope="row">Supervised by</th>
-                <td>{properties?.supervisor}</td>
-              </tr>
-            )}
-
-            {properties?.road_maint && (
-              <tr>
-                <th scope="row">Maintained by</th>
-                <td>{properties?.road_maint}</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-
-        <div
-          title="Download PDF"
-          className="download-icon"
-          onClick={downloadPdf}
-        >
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Download-Icon.png"
-            alt="download pdf"
-          />
+    <div className="section-container">
+      <div ref={tableContent}>
+        <div className="section-heading">
+          {properties?.LABEL} Road Properties
         </div>
+        <div className="section-table">
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Property</th>
+                <th scope="col">Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {properties?.Category_A && (
+                <tr>
+                  <th scope="row">Road Category</th>
+                  <td>{properties?.Category_A}</td>
+                </tr>
+              )}
+
+              {properties?.drainage_a && (
+                <tr>
+                  <th scope="row">Drainage</th>
+                  <td>{properties?.drainage_a}</td>
+                </tr>
+              )}
+
+              {properties?.LABEL && (
+                <tr>
+                  <th scope="row">Label</th>
+                  <td>{properties?.LABEL}</td>
+                </tr>
+              )}
+
+              {properties?.road_condi && (
+                <tr>
+                  <th scope="row">Road Condition</th>
+                  <td>{properties?.road_condi}</td>
+                </tr>
+              )}
+
+              {properties?.zone_name && (
+                <tr>
+                  <th scope="row">Zone name</th>
+                  <td>{properties?.zone_name}</td>
+                </tr>
+              )}
+
+              {properties?.supervisor && (
+                <tr>
+                  <th scope="row">Supervised by</th>
+                  <td>{properties?.supervisor}</td>
+                </tr>
+              )}
+
+              {properties?.road_maint && (
+                <tr>
+                  <th scope="row">Maintained by</th>
+                  <td>{properties?.road_maint}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div title="Download PDF" className="download-icon" onClick={downloadPdf}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Download-Icon.png"
+          alt="download pdf"
+        />
       </div>
     </div>
   );
